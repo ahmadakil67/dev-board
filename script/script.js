98af2df11cdb734bba56116b9cf7459d960dcbe8
Ahmad Akil <ferozbinakil@gmail.com>
2025-02-28 23:25:27 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,11 @@
+console.log("Before clicking alert button")
+alert("This is an alert!");
+
+console.log("Alert was acknowledged.");
+
+alert("Second alert!");
+
+function doSomething(){
+    console.log("doing something after the alert");
+}
+doSomething();
