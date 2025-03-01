11c78c935110e14ee70b45bd4bd9b1fc85e02775
Ahmad Akil <ferozbinakil@gmail.com>
2025-03-01 12:43:54 +0600

    Frontend Done

--- script/script.js
+++ script/script.js
@@ -1,11 +1,25 @@
-console.log("Before clicking alert button")
-alert("This is an alert!");
+document.getElementById('theme-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+    document.body.style.backgroundColor = getRandomColor();
 
-console.log("Alert was acknowledged.");
+    const randomColor = document.body.style.color;
+    const textColor = getContrastYIQ(randomColor);
 
-alert("Second alert!");
-
-function doSomething(){
-    console.log("doing something after the alert");
-}
-doSomething();
+    document.body.style.color = textColor;
+    function getContrastYIQ(hexcolor){
+        hexcolor = hexcolor.replace("#", "");
+        var r = parseInt(hexcolor.substr(0,2),16);
+        var g = parseInt(hexcolor.substr(2,2),16);
+        var b = parseInt(hexcolor.substr(4,2),16);
+        var yiq = ((r*299)+(g*587)+(b*114))/1000;
+        return (yiq >= 128) ? 'black' : 'white';
+    }
+});
